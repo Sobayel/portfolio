7b52cfb6748909ee340fe47d0b53f352fbddc251
mdsobayelhossain <mdsobayelhossain@gmail.com>
2024-07-04 18:38:56 +0600

    added contact section

--- js/script.js
+++ js/script.js
@@ -10,3 +10,27 @@ var typed = new Typed(".typing-2",{
     typeSpeed: 100,
     backSpeed: 60,
 });
+
+//Show/hide nav
+const menu = document.querySelector('.nav_menu');
+const menuBtn = document.querySelector('#open-menu-btn');
+const closeBtn = document.querySelector('#close-menu-btn');
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = "flex";
+    closeBtn.style.display = "inline-block";
+    menuBtn.style.display = "none";
+})
+
+//close nav
+const closeNav = () => {
+    menu.style.display = "none";
+    closeBtn.style.display = "none";
+    menuBtn.style.display = "inline-block";
+}
+closeBtn.addEventListener('click', closeNav);
+
+//nav scroll
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY>100);
+})
