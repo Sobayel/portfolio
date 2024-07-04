da03b7e15cc6f758486ecbaa0ddfb6bbe1a59ef0
mdsobayelhossain <mdsobayelhossain@gmail.com>
2024-07-04 14:45:17 +0600

    added header section

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+var typed = new Typed(".typing",{
+    strings: ["Front-End Developer", "Web Developer"],
+    typeSpeed: 80,
+    backSpeed: 80,
+});
+
+var typed = new Typed(".typing-2",{
+    strings: ["Web Developer", "Front-End Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+});
